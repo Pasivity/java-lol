10a217f080dc70fee4579bcae65fea4a8b216afe
Pasivity <www.rafa.lopex.11@gmail.com>
2022-12-25 18:20:46 -0800

    cool

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-function isNotZero(num) {
-  return num !== 0
+function getMax(arr) {
+  let max = arr[0];
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
 }
 
-console.log(isNotZero(1));
+console.log(getMax([1, 5, 4]));
